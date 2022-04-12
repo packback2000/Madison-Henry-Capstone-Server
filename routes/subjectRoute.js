935daf7bff6cac1a8d2347274bab1a98c74a3b78
Your Name <you@example.com>
2022-04-12 14:13:00 -0400

    fixed routes

--- routes/subjectRoute.js
+++ routes/subjectRoute.js
@@ -3,8 +3,13 @@ const subjectController = require('../controllers/subjectController');
 
 router.route('/').get(subjectController.index);
 
-router.route('/:id').get(subjectController.singleSubject);
+router.route('/:subject_id').get(subjectController.singleSubject);
 
 router.route('/').post(subjectController.addSubject);
+
+router
+    .route('/:subject_id/posts')
+    .get(subjectController.subjectInventory)
+    .post(subjectController.subjectInventory);
 
 module.exports = router;
